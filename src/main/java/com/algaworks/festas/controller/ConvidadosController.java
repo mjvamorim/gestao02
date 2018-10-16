f48173933df2b07e2d7a7e3587fa891549d3fb03
Mauricio Amorim <mjvamorim@gmail.com>
2018-10-16 20:57:34 -0300

    Salvando sem validacoes

--- src/main/java/com/algaworks/festas/controller/ConvidadosController.java
+++ src/main/java/com/algaworks/festas/controller/ConvidadosController.java
@@ -3,8 +3,10 @@ package com.algaworks.festas.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.festas.models.Convidado;
 import com.algaworks.festas.repository.Convidados;
 
 @Controller
@@ -17,7 +19,14 @@ public class ConvidadosController {
 	@RequestMapping("")
 	public ModelAndView listar() {
 		ModelAndView mv = new ModelAndView("ConvidadosListagem.html");
+		mv.addObject(new Convidado());
 		mv.addObject("convidados",convidados.findAll());
 		return mv;
+	}
+	
+	@RequestMapping(value="",method=RequestMethod.POST)
+	public String gravar(Convidado c) {
+		convidados.save(c);
+		return "redirect:/convidados";
 	}
 }
